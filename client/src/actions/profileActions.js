0bdf4b4be94ff7f486d2889ecbb148fd9853e3d3
Daniel Ale <danielale@MacBook-Pro-8.local>
2018-08-08 14:38:19 +0100

    IDM CLone

---
+++ client/src/actions/profileActions.js
@@ -0,0 +1,133 @@
+import axios from "axios";
+import {
+  GET_PROFILE,
+  PROFILE_LOADING,
+  CLEAR_CURRENT_PROFILE,
+  GET_ERRORS,
+  SET_CURRENT_USER
+} from "./types";
+
+// Add Experience
+export const addNewExperience = (expData, history) => dispatch => {
+  axios
+    .post("/api/profile/experience", expData)
+    .then(res => history.push("/dashboard"))
+    .catch(err => {
+      dispatch({ type: GET_ERRORS, payload: err.response.data });
+      console.log(err);
+    });
+};
+
+// Add Experience
+export const addNewEducation = (eduData, history) => dispatch => {
+  axios
+    .post("/api/profile/education", eduData)
+    .then(res => history.push("/dashboard"))
+    .catch(err => {
+      dispatch({ type: GET_ERRORS, payload: err.response.data });
+      console.log(err);
+    });
+};
+
+// Get Current Profile
+export const getCurrentProfile = () => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get("/api/profile")
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: {}
+      })
+    );
+};
+
+// Profile Loading
+export const setProfileLoading = () => {
+  return {
+    type: PROFILE_LOADING
+  };
+};
+
+// Clear Profile
+export const clearCurrentProfile = () => {
+  return {
+    type: CLEAR_CURRENT_PROFILE
+  };
+};
+
+// Create profile
+export const createProfile = (profileData, history) => dispatch => {
+  axios
+    .post("/api/profile", profileData)
+    .then(res => history.push("/dashboard"))
+    .catch(err => {
+      dispatch({ type: GET_ERRORS, payload: err.response.data });
+      console.log(err);
+    });
+};
+
+export const deleteEducation = id => dispatch => {
+  if (window.confirm("Are you sure? This cannot be undone!")) {
+    axios
+      .delete(`/api/profile//education/{id}`)
+      .then(res =>
+        dispatch({
+          type: GET_PROFILE,
+          payload: res.data
+        })
+      )
+      .catch(err => {
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        });
+      });
+  }
+};
+
+// Delete Experience
+export const deleteExperience = id => dispatch => {
+  if (window.confirm("Are you sure? This cannot be undone!")) {
+    axios
+      .delete(`/api/profile//experience/{id}`)
+      .then(res =>
+        dispatch({
+          type: GET_PROFILE,
+          payload: res.data
+        })
+      )
+      .catch(err => {
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        });
+      });
+  }
+};
+
+// Delete the account
+export const deleteAccount = () => dispatch => {
+  if (window.confirm("Are you sure? This cannot be undone!")) {
+    axios
+      .delete("/api/profile")
+      .then(res =>
+        dispatch({
+          type: SET_CURRENT_USER,
+          payload: {}
+        })
+      )
+      .catch(err => {
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        });
+      });
+  }
+};
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoUri: 'mongodb://ipnx:ipnx4321@ds115799.mlab.com:15799/devconnector',
+  secretOrKey: 'secret'
+}
